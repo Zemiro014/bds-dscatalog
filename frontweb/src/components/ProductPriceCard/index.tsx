220c0b00fa8ef64acfc7dde62bfb27ddcad17973
Zemiro014 <zemiro014@gmail.com>
2021-07-22 13:29:26 -0300

    Routes

---
+++ frontweb/src/components/ProductPriceCard/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+const ProductPriceCard = () => {
+  return (
+    <>
+      <div className="product-price-container">
+        <span>R$</span>
+        <h3>2345.67</h3>
+      </div>
+    </>
+  );
+};
+export default ProductPriceCard;
