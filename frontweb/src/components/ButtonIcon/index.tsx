62dde9319b6b81db37c6557e41e2d8b4e9790a88
Zemiro014 <zemiro014@gmail.com>
2021-07-22 14:28:04 -0300

    Custom ButtonIcon

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,10 +1,15 @@
-import './styles.css';
-
+import "./styles.css";
+import {ReactComponent as SetaIcon} from 'assets/images/Seta.svg';
 const ButtonIcon = () => {
-    return (
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA </h6>
+  return (
+    <div className="btn-container">
+        <button className="btn btn-primary">
+          <h6>INICIE AGORA A SUA BUSCA </h6>
         </button>
-    );
-}
+      <div className="btn-icon-container">
+        <SetaIcon />
+      </div>
+    </div>
+  );
+};
 export default ButtonIcon;
